7080813d09262f6ee9ef5380f8b6a41924e5437c
jairopanduro <jairo.l.panduro@gmail.com>
2018-06-18 12:03:01 +0300

    Trying to remove object Object from dangerously set html

--- src/Truncate.js
+++ src/Truncate.js
@@ -385,7 +385,7 @@ export default class Truncate extends Component {
             delete spanProps.useDangerouslySetInnerHTML;
             return (
                 <span {...spanProps} ref={(targetEl) => { this.elements.target = targetEl; }}>
-                    <span dangerouslySetInnerHTML={{__html: text}} />
+                    <span dangerouslySetInnerHTML={{__html: text.map(React.renderToStaticMarkup).join('') }} />
                     <span ref={(textEl) => { this.elements.text = textEl; }} >{children}</span>
                     <span ref={(ellipsisEl) => { this.elements.ellipsis = ellipsisEl; }} style={this.styles.ellipsis}>
                         {ellipsis}
